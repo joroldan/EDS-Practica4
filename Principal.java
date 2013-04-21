c465faf437e884864be948666ea8ea911a9efd1b
Jorge <jorge.es.iu@gmail.com>
2013-04-21 20:42:52 +0200

    Adaptado

--- Principal.java
+++ Principal.java
@@ -2,11 +2,6 @@ public class Principal
 {
 	public static void main(String[] args)
 	{
-		Suma s = new Suma(new Producto(new Opuesto(new ExpAtomica("x")),new ExpAtomica("y")),new Suma(new ExpAtomica(2),new ExpAtomica("x")));
-		Valoracion val = new Valoracion();
-		val.add("x",1);
-		val.add("y",0);
-		System.out.println("Cuando x=1 e y=0, entonces "+  s + " = " + s.valorar(val));
 		CalculadoraGrafica cg = new CalculadoraGrafica();
 	}
 }
